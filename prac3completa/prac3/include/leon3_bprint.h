bf9e17ab395c4812e3f3180d36f4bc616f310304
Edel Diaz <71083853+edeldiazll@users.noreply.github.com>
2022-03-23 11:52:40 +0100

    Add files via upload
    Limpio el repositorio y subo los mismos ficheros, pero sin estar comprimidos. Sólo subo los .h y .c

---
+++ prac3completa/prac3/include/leon3_bprint.h
@@ -0,0 +1,15 @@
+/*
+ * leon3_bprint.h
+ *
+ *  Created on: 02/03/2022
+ *      Author: ivan
+ */
+#ifndef LEON3_BPRINT_H_
+#define LEON3_BPRINT_H_
+
+#include "leon3_types.h"
+int8_t leon3_print_string(char* str);
+int8_t leon3_print_uint8(uint8_t i);
+
+#endif
+
---
+++ prac3completa/prac3b/src/leon3_bprint.c
@@ -0,0 +1,66 @@
+/*
+ * leon3_bprint.c
+ *
+ *  Created on: 02/03/2022
+ *      Author: ivan
+ */
+#include "leon3_bprint.h"
+
+int8_t leon3_print_string(char* str){
+
+	uint32_t write_timeout=0;
+	int cont = 0;
+	char car = (char) str[cont];
+
+	while(car != '\0'){
+		leon3_putchar(car);
+		cont = cont + 1;
+		car = (char) str[cont];
+	}
+	return (write_timeout == 0xAAAAA);
+}
+
+int8_t leon3_print_uint8(uint8_t i){
+
+	uint32_t write_timeout=0;
+
+	if(i>99){
+		leon3_putchar((i/100) + 48);
+		leon3_putchar(((i%100)/10)+48);
+		leon3_putchar((i%10)+48);
+	} else if (i>9){
+		leon3_putchar((i/10)+48);
+		leon3_putchar((i/10)+48);
+	} else {
+		leon3_putchar(i+48);
+	}
+	return (write_timeout == 0xAAAAA);
+}
+
+//uint8 [255-0]
+uint32_t leon3_print_uint32(uint32_t i){
+
+	int8_t error=0;
+	uint8_t aux = 1000000000;//2 elevado a 32
+	int8_t first_digit = 0;
+
+	if(i==0)
+		error=leon3_putchar('0');
+	else{
+		while(aux&&(!error)){
+			uint8_t digit;
+
+			digit=i/aux;
+			i-=aux*digit;
+			aux=aux/10;
+			if((first_digit)||digit){
+				error=leon3_putchar('0'+digit);
+				first_digit=1;
+			}
+		}
+	}
+
+	while(!leon3_uart_tx_fifo_is_empty());
+
+  return error;
+}
